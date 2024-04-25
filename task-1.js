f98855c56ee7a50f3640a6ec8da019a1996865e5
sebsto93 <s.stobiecki93@gmail.com>
2024-04-25 21:03:31 +0200

    task-2
    done

--- task-1.js
+++ task-1.js
@@ -8,3 +8,9 @@ function makeTransaction(quantity, pricePerDroid, customerCredits) {
   }
 }
 
+
+console.log(makeTransaction(5, 3000, 23000)); 
+console.log(makeTransaction(3, 1000, 15000)); 
+console.log(makeTransaction(10, 5000, 8000)); 
+console.log(makeTransaction(8, 2000, 10000)); 
+console.log(makeTransaction(10, 500, 5000)); 
---
+++ task-2.js
@@ -0,0 +1,14 @@
+function formatMessage(message, maxLenght) {
+    if (message.lenght <= maxLenght) {
+        return message;
+    } else {
+        return message.slice(0, maxLenght) + "...";
+    }
+}
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); 
+console.log(formatMessage("Curabitur ligula sapien", 23)); 
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); 
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); 
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); 
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); 
